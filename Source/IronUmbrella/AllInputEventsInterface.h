d4d584c3d7ea0985cae079026d693add8e35c378
Yorda <921864047@qq.com>
2022-01-09 17:14:46 +0900

    update

---
+++ Source/IronUmbrella/AllInputEventsInterface.h
@@ -0,0 +1,79 @@
+﻿/************************************************* 
+Copyright: All rights reserved for Shadow Candle Studio 
+Author: Junlin Sun
+Date:2022-01-9 
+Description: This file contains the declaration of: UAllInputEventsInterface, and IAllInputEventsInterface
+**************************************************/ 
+#pragma once
+
+//include files
+#include "CoreMinimal.h"
+#include "AllInputEventsInterface.generated.h"
+
+
+//Macro for generating press event functions
+#define GenerateFunctionsForOneButton(ButtonName) \
+UFUNCTION(BlueprintCallable,BlueprintImplementableEvent,Category="ButtonAction")\
+void LightPressStartEvent_##ButtonName();\
+UFUNCTION(BlueprintCallable,BlueprintImplementableEvent,Category="ButtonAction")\
+void LightPressOverEvent_##ButtonName(float Duration);\
+UFUNCTION(BlueprintCallable,BlueprintImplementableEvent,Category="ButtonAction")\
+void ChargePressStartEvent_##ButtonName();\
+UFUNCTION(BlueprintCallable,BlueprintImplementableEvent,Category="ButtonAction")\
+void ChargePressOverEvent_##ButtonName(float Duration);\
+
+//Macro for generating Axis event functions
+#define GenerateAxisFunction(AxisName,DimensionName)\
+UFUNCTION(BlueprintCallable,BlueprintImplementableEvent,Category="ButtonAction") void AxisName##AxisEvent_##DimensionName(float DimensionName##Axis);
+
+
+//Reflection identifier
+UINTERFACE(Blueprintable)
+class IRONUMBRELLA_API UAllInputEventsInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+//Declaration class of the interface,in which most of the events needed in our input system are declared
+class IAllInputEventsInterface
+{
+	GENERATED_BODY()
+	
+public:
+	
+
+	//Lstick functions declaration
+	GenerateAxisFunction(LStick,X)
+	GenerateAxisFunction(LStick,Y)
+	GenerateFunctionsForOneButton(LStick)
+
+	//Rstick functions declaration
+	GenerateAxisFunction(RStick,X)
+	GenerateAxisFunction(RStick,Y)
+	GenerateFunctionsForOneButton(RStick)
+
+	//Right group buttons functions declaration
+	GenerateFunctionsForOneButton(RBG_LEFT)
+	GenerateFunctionsForOneButton(RBG_RIGHT)
+	GenerateFunctionsForOneButton(RBG_TOP)
+	GenerateFunctionsForOneButton(RBG_DOWN)
+
+	//Left group buttons functions declaration
+	GenerateFunctionsForOneButton(LBG_LEFT)
+	GenerateFunctionsForOneButton(LBG_RIGHT)
+	GenerateFunctionsForOneButton(LBG_TOP)
+	GenerateFunctionsForOneButton(LBG_DOWN)
+
+	//Special buttons functions declaration
+	GenerateFunctionsForOneButton(SP_LEFT)
+	GenerateFunctionsForOneButton(SP_RIGHT)
+
+	//Shoulder buttons functions declaration
+	GenerateFunctionsForOneButton(LB)
+	GenerateFunctionsForOneButton(RB)
+
+	//Trigger buttons functions declaration
+	GenerateFunctionsForOneButton(RT)
+	GenerateFunctionsForOneButton(LT)
+	;
+};
---
+++ Source/IronUmbrella/Controllable2DPawnBase.h
@@ -0,0 +1,26 @@
+﻿/************************************************* 
+Copyright: All rights reserved for Shadow Candle Studio 
+Author: Junlin Sun
+Date:2022-01-9 
+Description: This file contains the declaration of: TriggerbleEventInterface,  AControllable2DPawnBase
+**************************************************/ 
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AllInputEventsInterface.h"
+#include "Pixel2DCharacter.h"
+#include "Controllable2DPawnBase.generated.h"
+/**
+ * 
+ */
+UCLASS(BlueprintType,Blueprintable,ShowCategories=("Pixel2D character"))
+class IRONUMBRELLA_API AControllable2DPawnBase : public APixel2DCharacter, public IAllInputEventsInterface
+{
+	GENERATED_BODY()
+
+public:
+	//
+	void BindAllInputFromIAllInputEventsInterface();
+
+	
+};
