60f7a75e12969880bc906259d0b7f46728f5b6ca
Yorda <921864047@qq.com>
2022-01-14 11:35:48 +0900

    movement component update!

---
+++ Source/IronUmbrella/Components/BP_MovementComponentBase.cpp
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BP_MovementComponentBase.h"
+
+
+// Sets default values for this component's properties
+UBP_MovementComponentBase::UBP_MovementComponentBase()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = false;
+	
+	// ...
+}
+
+
+void UBP_MovementComponentBase::Move_Implementation(APawn* Cha, float MoveValue)
+{
+	
+}
+
+// Called when the game starts
+void UBP_MovementComponentBase::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UBP_MovementComponentBase::TickComponent(float DeltaTime, ELevelTick TickType,
+                                              FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
---
+++ Source/IronUmbrella/Components/BP_MovementComponentBase.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+
+#include "BP_MovementComponentBase.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class IRONUMBRELLA_API UBP_MovementComponentBase : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UBP_MovementComponentBase();
+	UPROPERTY(BlueprintReadWrite,EditAnywhere)
+	uint8 JumpMaxCountLimit;
+	UFUNCTION(BlueprintCallable,BlueprintNativeEvent)
+	void Move(APawn* Cha,float MoveValue);
+	void Move_Implementation(APawn* Cha,float MoveValue);
+	
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	class UCharacterMovementComponent* ChaMovementCompoRef;
+	AActor* OwningActor;
+	FTimerHandle DashTimerHanle;
+	FTimerHandle JumpTimerHandle;
+	BYTE JumpCount;
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+};
