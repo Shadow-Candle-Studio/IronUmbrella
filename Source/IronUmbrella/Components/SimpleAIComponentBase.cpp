46c7f35941a6753481da6733e3b51870336fb884
Yorda <921864047@qq.com>
2022-02-15 14:33:32 +0900

    ai updates

--- Source/IronUmbrella/Components/SimpleAIComponentBase.cpp
+++ Source/IronUmbrella/Components/SimpleAIComponentBase.cpp
@@ -3,14 +3,18 @@
 
 #include "SimpleAIComponentBase.h"
 
+#include "IronUmbrella/AllStructuresAndEnums.h"
+#include "IronUmbrella/IronMobileObject.h"
 
+const FName USimpleAIComponentBase::PlayerTag=FName(TEXT("MainPlayer"));
 // Sets default values for this component's properties
 USimpleAIComponentBase::USimpleAIComponentBase()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
-
+	PrimaryComponentTick.bCanEverTick = false;
+	
+	
 	// ...
 }
 
@@ -19,18 +23,92 @@ USimpleAIComponentBase::USimpleAIComponentBase()
 void USimpleAIComponentBase::BeginPlay()
 {
 	Super::BeginPlay();
+	OnComponentBeginOverlap.AddDynamic(this,&USimpleAIComponentBase::WhenComponentBeginOverlap);
+	OnComponentEndOverlap.AddDynamic(this,&USimpleAIComponentBase::WhenComponentEndOverlap);
+	WhenPlayerActorPointerValueChanged.AddDynamic(this,&USimpleAIComponentBase::WhenPlayerPointerValueChanged);
 
+	
+	OwningCharacter=GetOwner();
+	if(OwningCharacter==nullptr)
+	{
+		YellSomething("Ai pawn owner getting failure")
+	}
+	else
+	{
+		AIronMobileObject* Cha=Cast<AIronMobileObject>(OwningCharacter);
+		if(Cha!=nullptr)
+		{
+			
+		}
+	}
+
+	
 	// ...
 	
 }
 
-
-// Called every frame
-void USimpleAIComponentBase::TickComponent(float DeltaTime, ELevelTick TickType,
-                                           FActorComponentTickFunction* ThisTickFunction)
+void USimpleAIComponentBase::SetPlayerPointer(AActor* in_Pplayer)
 {
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	// ...
+	if(in_Pplayer!=this->PlayerPointer)
+	{
+		this->PlayerPointer=in_Pplayer;
+		WhenPlayerActorPointerValueChanged.Broadcast();
+	}
 }
+
+void USimpleAIComponentBase::AiUpdateEvent_Implementation()
+{
+	;
+}
+
+void USimpleAIComponentBase::WhenPlayerPointerValueChanged_Implementation()
+{
+	//Player detected
+	if(this->PlayerPointer!=nullptr)
+	{
+		++PlayerCommingToMyFieldTimes;
+		//timer exists so you only need to unpause it and use it
+		if(GetWorld()->GetTimerManager().TimerExists(AiUpdateTimer))
+		{
+			GetWorld()->GetTimerManager().UnPauseTimer(AiUpdateTimer);
+		}
+		else
+		{
+			GetWorld()->GetTimerManager().SetTimer
+			(AiUpdateTimer,this,
+				&USimpleAIComponentBase::AiUpdateEvent,
+				AiUpdateTimerSpeed,
+				true,
+				-1);
+		}
+	}
+	else //player coming out of field or dead
+	{
+		if(GetWorld()->GetTimerManager().TimerExists(AiUpdateTimer))
+		{
+			GetWorld()->GetTimerManager().PauseTimer(AiUpdateTimer);
+		}
+	}
+}
+
+void USimpleAIComponentBase::WhenComponentBeginOverlap_Implementation(UPrimitiveComponent* OverlappedComponent,
+                                                                      AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+                                                                      const FHitResult& SweepResult)
+{
+	if(OtherActor!=nullptr&&OtherActor->ActorHasTag(PlayerTag))
+	{
+		SetPlayerPointer(OtherActor);
+	}
+}
+
+
+void USimpleAIComponentBase::WhenComponentEndOverlap_Implementation(UPrimitiveComponent* OverlappedComponent,
+	AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if(OtherActor!=nullptr&&OtherActor->ActorHasTag(PlayerTag))
+	{
+		SetPlayerPointer(nullptr);
+	}
+}
+
 
--- Source/IronUmbrella/Components/SimpleAIComponentBase.h
+++ Source/IronUmbrella/Components/SimpleAIComponentBase.h
@@ -4,24 +4,49 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Components/BoxComponent.h"
 #include "SimpleAIComponentBase.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnPlayerActorPointerValueChanged);
 
-UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent),Blueprintable,BlueprintType)
-class IRONUMBRELLA_API USimpleAIComponentBase : public UActorComponent
+UCLASS(ClassGroup=(Custom),Blueprintable,BlueprintType)
+class IRONUMBRELLA_API USimpleAIComponentBase : public UBoxComponent
 {
 	GENERATED_BODY()
 
 public:
 	// Sets default values for this component's properties
 	USimpleAIComponentBase();
-
+private:
+	AActor* PlayerPointer;
+	FTimerHandle AiUpdateTimer;
+	int PlayerCommingToMyFieldTimes=0;
 protected:
+	FOnPlayerActorPointerValueChanged WhenPlayerActorPointerValueChanged;
+	const static FName PlayerTag;
+	AActor* OwningCharacter;
 	// Called when the game starts
 	virtual void BeginPlay() override;
-
+	
 public:
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
-	                           FActorComponentTickFunction* ThisTickFunction) override;
+	//Speed of the ai updating timer when player comes into the field of a monster
+	UPROPERTY(BlueprintReadWrite,EditAnywhere)
+	float AiUpdateTimerSpeed=0.1f;
+	UFUNCTION(BlueprintCallable)
+	void SetPlayerPointer(AActor* in_Pplayer);
+	UFUNCTION(BlueprintCallable)
+	AActor* GetPlayerPointer() const
+	{
+		return PlayerPointer;
+	}
+	UFUNCTION(BlueprintNativeEvent)
+	void WhenComponentBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
+	UFUNCTION(BlueprintNativeEvent)
+	void WhenComponentEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	UFUNCTION(BlueprintNativeEvent)
+	void WhenPlayerPointerValueChanged();
+
+	UFUNCTION(BlueprintNativeEvent)
+	void AiUpdateEvent();
 };
